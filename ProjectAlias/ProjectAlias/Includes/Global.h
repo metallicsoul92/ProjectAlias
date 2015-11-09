d0d4d44ee23dbf292c8092f938325dfb075fcab2
metallicsoul92 <metallicsoul92@aim.com>
2015-11-08 21:18:10 -0500

    Initial Commit

---
+++ ProjectAlias/ProjectAlias/Includes/Global.h
@@ -0,0 +1,10 @@
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+
+#endif GLOBAL_H_
---
+++ ProjectAlias/ProjectAlias/Source/main.c
@@ -0,0 +1,4 @@
+#include "../Includes/Global.h"
+
+
+
