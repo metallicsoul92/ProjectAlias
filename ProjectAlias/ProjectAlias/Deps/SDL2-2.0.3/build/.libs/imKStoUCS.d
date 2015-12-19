2e4415c3fbc5f9ef20d206183c610d22f35af7c1
Clim <Metallicsoul92@aim.com>
2015-12-18 23:37:17 -0500

    SDLNET LINUX

---
+++ ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/build/.libs/imKStoUCS.d
@@ -0,0 +1,8 @@
+build/imKStoUCS.lo: \
+ /home/climjark/ProjectAlias/ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/src/video/x11/imKStoUCS.c \
+ /home/climjark/ProjectAlias/ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/src/video/x11/../../SDL_internal.h \
+ /home/climjark/ProjectAlias/ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/src/video/x11/../../dynapi/SDL_dynapi.h \
+ /home/climjark/ProjectAlias/ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/src/video/x11/../../dynapi/SDL_dynapi_overrides.h \
+ include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+ include/close_code.h \
+ /home/climjark/ProjectAlias/ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/src/video/x11/imKStoUCS.h
---
+++ ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/build/edid-parse.d
@@ -0,0 +1,3 @@
+build/edid-parse.lo: \
+ /home/climjark/ProjectAlias/ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/src/video/x11/edid-parse.c \
+ /home/climjark/ProjectAlias/ProjectAlias/ProjectAlias/Deps/SDL2-2.0.3/src/video/x11/edid.h
